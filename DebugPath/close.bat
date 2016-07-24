9bb595fcd6c63e0ea553ccf76f6fe4eac057f3f1
CrazyPro <dplxl@163.com>
2016-07-24 20:36:12 +0800

    .gitignore

--- DebugPath/close.bat
+++ DebugPath/close.bat
@@ -1,3 +1,4 @@
 taskkill /IM worldd.exe
 taskkill /IM gamed.exe
 taskkill /IM platd.exe
+taskkill /IM countryd.exe
--- DebugPath/run.bat
+++ DebugPath/run.bat
@@ -1,4 +1,5 @@
 start worldd 1
 start platd 1
+start countryd 1
 start gamed 1 
 start gamed 2
